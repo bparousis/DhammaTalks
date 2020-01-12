4608770b8fe8f16606651d46fa34483e784ef581
Bill Parousis <bparousis.appdev@gmail.com>
2020-01-11 19:14:42 -0500

    Initial Commit

---
+++ DhammaTalks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DhammaTalks
+//
+//  Created by Bill Parousis on 2020-01-11.
+//  Copyright © 2020 Bill Parousis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
