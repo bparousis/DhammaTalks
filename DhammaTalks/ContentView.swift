21da9e3871edd311d5d15106581dc466e8f6ab2d
Bill Parousis <bparousis@gmail.com>
2021-11-20 12:41:35 -0500

    Fixed grid layout for talk series selector and moved it to its own file.

--- DhammaTalks/ContentView.swift
+++ DhammaTalks/ContentView.swift
@@ -10,11 +10,6 @@ import SwiftUI
 import AVFoundation
 
 struct ContentView: View {
-    
-    let columns = [
-        GridItem(.adaptive(minimum: 150)),
-        GridItem(.adaptive(minimum: 150))
-    ]
 
     var body: some View {
         TabView {
@@ -26,27 +21,13 @@ struct ContentView: View {
             }
             
             NavigationView {
-                LazyVGrid(columns: columns, spacing: 10) {
-                    if let talkSeriesList = TalkDataService.talkSeriesList {
-                        ForEach(talkSeriesList) { talkSeries in
-                            NavigationLink(destination: TalkSeriesListView(talkSeries:talkSeries)) {
-                                ZStack{
-                                    Image(talkSeries.image)
-                                        .resizable()
-                                        .frame(width: 200, height:100, alignment:.center)
-                                        .aspectRatio(contentMode:.fill)
-                                        .cornerRadius(20)
-                                    Text(talkSeries.title)
-                                        .bold()
-                                        .font(.system(size:18, weight:.bold))
-                                        .foregroundColor(.white)
-                                        .frame(width: 180, height:100, alignment:.center)
-                                }
-                            }
-                        }
-                    }
+                if let talkSeriesList = TalkDataService.talkSeriesList {
+                    TalkSeriesSelectorView(talkSeriesList: talkSeriesList)
+                } else {
+                    // It's not expected that this would ever be shown, since TalkDataService.talkSeriesList should
+                    // load talk series from JSON file and display it in TalkSeriesSelectorView.
+                    Text("No talk collections found")
                 }
-                .navigationTitle("Collections")
             }
             .tabItem {
                 Label("Collections", systemImage: "square.grid.2x2")
