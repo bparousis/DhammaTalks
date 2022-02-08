b9d8db339938ad9eff44538dbe6cd8e00a00c08c
Bill Parousis <bparousis@gmail.com>
2022-02-07 22:50:32 -0500

    Added basic settings/about screen (#27)
    * Added basic settings/about screen for showing app version, link to site and questions/feedback mail option.
    
    * Minor cleanup to code.

---
+++ DhammaTalks/Settings/MailView.swift
@@ -0,0 +1,61 @@
+//
+//  MailView.swift
+//  DhammaTalks
+//
+//  Created by Bill Parousis on 2022-02-07.
+//  Copyright © 2022 Bill Parousis. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+import MessageUI
+
+struct MailView: UIViewControllerRepresentable {
+
+    @Binding var isShowing: Bool
+    @Binding var result: Result<MFMailComposeResult, Error>?
+
+    class Coordinator: NSObject, MFMailComposeViewControllerDelegate {
+
+        @Binding var isShowing: Bool
+        @Binding var result: Result<MFMailComposeResult, Error>?
+
+        init(isShowing: Binding<Bool>,
+             result: Binding<Result<MFMailComposeResult, Error>?>) {
+            _isShowing = isShowing
+            _result = result
+        }
+
+        func mailComposeController(_ controller: MFMailComposeViewController,
+                                   didFinishWith result: MFMailComposeResult,
+                                   error: Error?) {
+            defer {
+                isShowing = false
+            }
+            
+            if let error = error {
+                self.result = .failure(error)
+            } else {
+                self.result = .success(result)
+            }
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(isShowing: $isShowing,
+                           result: $result)
+    }
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<MailView>) -> MFMailComposeViewController {
+        let vc = MFMailComposeViewController()
+        vc.setSubject("DhammaTalks: Questions & Feedback")
+        vc.setToRecipients(["accesstoinsightapp@gmail.com"])
+        vc.mailComposeDelegate = context.coordinator
+        return vc
+    }
+
+    func updateUIViewController(_ uiViewController: MFMailComposeViewController,
+                                context: UIViewControllerRepresentableContext<MailView>) {
+
+    }
+}
