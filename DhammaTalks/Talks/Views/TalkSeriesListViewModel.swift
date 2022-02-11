f88d92054ffecfabc97ce1049cea12315187eab7
Bill Parousis <bparousis@gmail.com>
2022-02-10 23:28:26 -0500

    Added search to talk series list view.

---
+++ DhammaTalks/Talks/Views/TalkSeriesListViewModel.swift
@@ -0,0 +1,55 @@
+//
+//  TalkSeriesListViewModel.swift
+//  DhammaTalks
+//
+//  Created by Bill Parousis on 2022-02-10.
+//  Copyright © 2022 Bill Parousis. All rights reserved.
+//
+
+import Foundation
+
+
+class TalkSeriesListViewModel: ObservableObject {
+
+    private let talkSeries: TalkSeries
+    private let talkUserInfoService: TalkUserInfoService
+    private let downloadManager: DownloadManager
+
+    var title: String {
+        talkSeries.title
+    }
+    
+    var description: String {
+        talkSeries.description
+    }
+    
+    @Published private(set) var talkSections: [TalkSectionViewModel] = []
+    
+    init(talkSeries: TalkSeries, talkUserInfoService: TalkUserInfoService, downloadManager: DownloadManager) {
+        self.talkSeries = talkSeries
+        self.talkUserInfoService = talkUserInfoService
+        self.downloadManager = downloadManager
+    }
+    
+    func fetchData(searchText: String = "") {
+        var talkSectionViewModelList: [TalkSectionViewModel] = []
+        let searchTextLowerCased = searchText.lowercased()
+        for section in talkSeries.sections {
+            let sectionViewModel = TalkSectionViewModel(id: section.id, title: section.title)
+            
+            var talkRows: [TalkData] = section.talks
+            if !searchText.isEmpty {
+                talkRows = talkRows.filter { $0.title.lowercased().contains(searchTextLowerCased) }
+            }
+            
+            if !talkRows.isEmpty {
+                sectionViewModel.talkRows = talkRows.map{
+                    TalkRowViewModel(talkData: $0, talkUserInfoService: talkUserInfoService, downloadManager: downloadManager)
+                }
+                
+                talkSectionViewModelList.append(sectionViewModel)
+            }   
+        }
+        self.talkSections = talkSectionViewModelList
+    }
+}
