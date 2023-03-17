02bfc3b556a4e3873976ee84154c47b4ff0ff22d
Bill Parousis <bparousis@gmail.com>
2023-03-17 18:48:50 -0400

    Adding TalkData change

--- DhammaTalks/Talks/Data/TalkData.swift
+++ DhammaTalks/Talks/Data/TalkData.swift
@@ -13,8 +13,8 @@ struct TalkData: Identifiable, Decodable {
     let id: String
     let title: String
     let url: String
-    
-    init(id: String, title: String, url: String) {
+
+    init(id: String = UUID().uuidString, title: String, url: String) {
         self.id = id
         self.title = title
         self.url = url
